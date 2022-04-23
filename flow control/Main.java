83cdd9851f4387c8f82685650d380ba72bfd5301
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2022-04-23 15:12:18 +0530

    Done

--- flow control/Main.java
+++ flow control/Main.java
@@ -1,9 +1,21 @@
-package flow control;
-
 public class Main {
     public static void main(String[] args) {
         
         int i = 10;
+
+        /*
+        if: if statement is the most simple decision-making statement. 
+        It is used to decide whether a certain statement or block of statements will be executed or not i.e if a certain condition is true then a block of statement is executed otherwise not. 
+
+          Syntax: 
+
+           if(condition) 
+            {
+             // Statements to execute if
+             // condition is true
+            }
+
+        */
  
         if (i > 15)
             System.out.println("10 is less than 15");
